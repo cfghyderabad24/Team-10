5e63cf11e09c3f606c0e708defd3ad2dfd84fe0f
Yakshith15 <daggupatiyakshithnaidu@gmail.com>
2024-06-22 17:33:22 +0530

    signIn-signUp-stateManagement

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,18 @@
-
+import "flowbite/dist/flowbite.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Signup } from "./pages/SignUp";
+import { Signin } from "./pages/SignIn";
 function App() {
-
   return (
     <>
-      hello
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/signup" element={<Signup />} /> */}
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
