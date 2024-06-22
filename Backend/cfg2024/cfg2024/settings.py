dab7397080da92b042165a3196051c54611cab18
Harsha <rharsha171@gmail.com>
2024-06-22 15:30:15 +0530

    .env file updated

--- Backend/cfg2024/cfg2024/settings.py
+++ Backend/cfg2024/cfg2024/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#2@=!q4pso3hotl1@1ikrugc_g&^itpikosy*4vjr$yrapnw(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
